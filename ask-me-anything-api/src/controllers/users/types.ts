5ab004333cba94952f1cf23118f626a93989bffb
VoN <sudo.von.contact@gmail.com>
2025-02-27 22:11:01 -0600

    feat: :label: add controller detailed user types

--- ask-me-anything-api/src/controllers/users/types.ts
+++ ask-me-anything-api/src/controllers/users/types.ts
@@ -1,7 +1,10 @@
 import { components } from '@sudo-von/ask-me-anything-core';
 
 export type CreateUser = components['schemas']['CreateUser'];
+export type CreateDetailedUser = components['schemas']['CreateDetailedUser'];
 
 export type UpdateUser = components['schemas']['UpdateUser'];
+export type UpdateDetailedUser = components['schemas']['UpdateDetailedUser'];
 
 export type User = components['schemas']['User'];
+export type DetailedUser = components['schemas']['DetailedUser'];
