609b7904d68ba87bca5d3fe80bd842dc7fa8db6b
VoN <sudo.von.contact@gmail.com>
2025-02-27 22:16:50 -0600

    feat: :label: add controller user metrics types

--- ask-me-anything-api/src/controllers/users/types.ts
+++ ask-me-anything-api/src/controllers/users/types.ts
@@ -1,13 +1,17 @@
 import { components } from '@sudo-von/ask-me-anything-core';
 
-export type CreateUser = components['schemas']['CreateUser'];
 export type CreateDetailedUser = components['schemas']['CreateDetailedUser'];
+export type CreateUser = components['schemas']['CreateUser'];
 export type CreateUserDetails = components['schemas']['CreateUserDetails'];
-
-export type UpdateUser = components['schemas']['UpdateUser'];
-export type UpdateDetailedUser = components['schemas']['UpdateDetailedUser'];
-export type UpdateUserDetails = components['schemas']['UpdateUserDetails'];
+export type CreateUserMetrics = components['schemas']['CreateUserMetrics'];
 
 export type DetailedUser = components['schemas']['DetailedUser'];
+
+export type UpdateDetailedUser = components['schemas']['UpdateDetailedUser'];
+export type UpdateUser = components['schemas']['UpdateUser'];
+export type UpdateUserDetails = components['schemas']['UpdateUserDetails'];
+export type UpdateUserMetrics = components['schemas']['UpdateUserMetrics'];
+
 export type User = components['schemas']['User'];
 export type UserDetails = components['schemas']['UserDetails'];
+export type UserMetrics = components['schemas']['UserMetrics'];
