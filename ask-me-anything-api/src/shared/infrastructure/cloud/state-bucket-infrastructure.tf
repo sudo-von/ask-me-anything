a85726b4eb63c91a1269b79f551238bff22cf6bb
Jesús Ángel Rodríguez Martínez <sudo.von.contact@gmail.com>
2025-04-11 00:07:58 -0600

    build: :construction_worker: update working directory

---
+++ ask-me-anything-api/src/shared/infrastructure/cloud/state-bucket-infrastructure.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = ">= 1.2.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.16.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "ask_me_anything_state_bucket" {
+  bucket = "ask-me-anything-state-bucket"
+}
